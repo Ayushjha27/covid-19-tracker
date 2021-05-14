42d08b95b63ffb9b1e967d6274d0dd87504496d1
Ayush <ayushjha3210@gmail.com>
2021-05-14 11:26:57 +0530

    commited

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD6BrZDC8xolHLybmjNupU6uba8_x6Vhuk",
+    authDomain: "covid19-tracker-ce47a.firebaseapp.com",
+    projectId: "covid19-tracker-ce47a",
+    storageBucket: "covid19-tracker-ce47a.appspot.com",
+    messagingSenderId: "846138368455",
+    appId: "1:846138368455:web:ab50a54a4f8f2b2be9f9e2"
+};
